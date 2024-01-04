5e907c3e1c56f4d5f43d0c90d6141e1a3f6e1a0a
Mestu Paul <mestu.paul@orbitax.com>
2024-01-04 15:05:05 +0600

    end of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // routing to {base}/api/users
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers(){
+        var users = _context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("{id}")] // {base}/api/users/2
+    public ActionResult<AppUser>GetUser(int id){
+        return _context.Users.Find(id);
+    }
+}
