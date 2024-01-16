ba323aab961930f30ffba89712b671335b36af49
Mestu Paul <mestu.paul@orbitax.com>
2024-01-16 15:41:38 +0600

    end of section 6 with toastr error

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const accountService = inject(AccountService);
+  const toastr = inject(ToastrService);
+
+  return accountService.currentUser$.pipe(
+    map(user => {
+      if(user){
+        console.log("Calling me valid user",user);
+        return true;
+      }
+      else{
+        console.log("Calling me invalid user",user);
+        toastr.error('you shall not pass!');
+        return false;
+      }
+    })
+  )
+};
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -32,8 +30,7 @@ import { ListsComponent } from './lists/lists.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [
     provideClientHydration()
